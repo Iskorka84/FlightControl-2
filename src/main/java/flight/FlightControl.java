389d75f532c798e7caa0d5e6b8bc6802e59e1442
operchuk <liliya.iskorka@gmail.com>
2022-12-23 10:55:55 +0200

    New commit

--- src/main/java/flight/FlightControl.java
+++ src/main/java/flight/FlightControl.java
@@ -2,9 +2,70 @@ package flight;
 
 public class FlightControl {
 
-    public void boardingNewPassengers (Plane[] allPlanes, String[][] passengerList) {
-        for (int i = 0; i < allPlanes.length; i++) {
-               allPlanes[i].setNewPassengers(allPlanes, allPlanes[i], passengerList[i]);
+    final static int MAX_AIRPORT_CLIENTS = 11;
+
+    public Plane[] planes;
+    public String[] passengerNames = new String[MAX_AIRPORT_CLIENTS];
+    public int[] passengerSeats = new int[MAX_AIRPORT_CLIENTS];
+    public String[] passengerFlights = new String[MAX_AIRPORT_CLIENTS];
+
+    public FlightControl(Plane[] airportPlanes) {
+        planes = airportPlanes;
+    }
+
+    public Plane getPlaneByFlightId(String flightId) {
+        for (int i = 0; i < planes.length; i++) {
+            if (planes[i].flightID.equals(flightId)) {
+                return planes[i];
+            }
+        }
+        return null;
+    }
+
+       public void setPlaneDestination(String flightID, String destination) {
+        Plane plane = getPlaneByFlightId(flightID);
+        plane.setFlightDestination(destination);
+    }
+
+    public String getPlaneDestination(String flightID) {
+        Plane plane = getPlaneByFlightId(flightID);
+        String destination = plane.getFlightDestination();
+        return destination;
+    }
+
+    public void boardingNewPassenger(int passengerID) {
+        String passengerName = passengerNames[passengerID];
+        int passengerSeat = passengerSeats[passengerID];
+        String flightID = passengerFlights[passengerID];
+
+        Plane plane = getPlaneByFlightId(flightID);
+        plane.boardingPassenger(passengerName, passengerSeat);
+    }
+
+    public void boardingPassengerOnAlternativePlane (int passengerID) {
+        String passengerName = passengerNames[passengerID];
+        String flightID = passengerFlights[passengerID];
+
+        String destination = getPlaneDestination(flightID);
+        Plane alternativePlane = getAnotherPlaneWithSameDestination(flightID, destination);
+        if (alternativePlane != null) {
+            alternativePlane.offerFreeSeat(passengerName);
         }
     }
+
+    public void registerPassenger(int passengerID, String passengerName, int passengerSeat, String flightID) {
+        passengerNames[passengerID] = passengerName;
+        passengerSeats[passengerID] = passengerSeat;
+        passengerFlights[passengerID] = flightID;
+    }
+
+    public Plane getAnotherPlaneWithSameDestination(String flightID, String destination) {
+        for (int i = 0; i < planes.length; i++) {
+            if (!planes[i].flightID.equals(flightID) && planes[i].flightDestination.equals(destination)) {
+                return planes[i];
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/flight/Main.java
+++ src/main/java/flight/Main.java
@@ -4,28 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FlightControl boarding = new FlightControl();
+        Plane[] airportPlanes = new Plane[3];
+        airportPlanes[0] = new Plane("PS7701", 4);
+        airportPlanes[1] = new Plane("PS7702", 4);
+        airportPlanes[2] = new Plane("PS7703", 3);
 
-        Plane airbus = new Plane("airbus", 4);
-        Plane boeing = new Plane("boeing", 4);
-        Plane privateJet = new Plane("privateJet", 3);
+        FlightControl airport = new FlightControl(airportPlanes);
+        airport.setPlaneDestination("PS7701", "London");
+        airport.setPlaneDestination("PS7702", "Paris");
+        airport.setPlaneDestination("PS7703", "London");
 
-        Plane[] planes = {airbus, boeing, privateJet};
+        createPassengers(airport);
 
-        String[] firstAirbusPassengers = {"Alain Delon", "Tom Cruise", null, "Elvis Presley"};
-        String[] firstBoeingPassengers = {"David Beckham", "Leo Messi", null, null};
-        String[] firstPrivateJetPassengers = {"Joe Biden", null, null};
-        String[][] firstPlanePassengers = {firstAirbusPassengers, firstBoeingPassengers, firstPrivateJetPassengers};
+        for (int i = 0; i < airport.passengerNames.length; i++) {
+            airport.boardingNewPassenger(i);
+            if (!Plane.freeSeat) {
+                airport.boardingPassengerOnAlternativePlane(i);
+            }
+        }
 
-        boarding.boardingNewPassengers(planes, firstPlanePassengers);
+    }
 
-        String[] secondAirbusPassengers = {null, "Nicole Kidman", "Elvis Presley", null};
-        String[] secondBoeingPassengers = {null, "Diego Maradona", "Zinedine Zidane", "Andrii Shevchenko"};
-        String[] secondPrivateJetPassengers = {"Boris Johnson", null, "Mister X"};
-        String[][] secondPlanePassengers = {secondAirbusPassengers, secondBoeingPassengers, secondPrivateJetPassengers};
+    public static void createPassengers (FlightControl airport) {
+        airport.registerPassenger(0, "Alain Delon", 0, "PS7701");
+        airport.registerPassenger(1, "Tom Cruise", 1, "PS7701");
+        airport.registerPassenger(2, "Elvis Presley", 2, "PS7701");
+        airport.registerPassenger(3, "Nicole Kidman", 3, "PS7701");
 
-        boarding.boardingNewPassengers(planes, secondPlanePassengers);
+        airport.registerPassenger(4, "David Beckham", 0, "PS7702");
+        airport.registerPassenger(5, "Leo Messi", 1, "PS7702");
+        airport.registerPassenger(6, "Diego Maradona", 2, "PS7702");
+        airport.registerPassenger(7, "Zinedine Zidane", 5, "PS7702");
 
-
+        airport.registerPassenger(8, "Andrii Shevchenko", 0, "PS7703");
+        airport.registerPassenger(9, "Boris Johnson", 1, "PS7703");
+        airport.registerPassenger(10, "Mister X", 3, "PS7701");
     }
 }
